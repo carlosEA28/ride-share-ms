7e52005ce4a53ec50f6cb70db5575c91b3367e7c
carlosEA28 <carloseaduarte28@gmail.com>
2026-09-16 09:40:06 -0300

    feat: add initial grpc configuration

--- shared/contracts/amqp.go
+++ shared/contracts/amqp.go
@@ -15,7 +15,7 @@ const (
 	TripEventDriverNotInterested = "trip.event.driver_not_interested"
 
 	// Driver commands (driver.cmd.*)
-	DriverCmdTripRequest = "driver.cmd.trip_request"
+	DriverCmdTripRequest = "driver.cmd.trip_request"d
 	DriverCmdTripAccept  = "driver.cmd.trip_accept"
 	DriverCmdTripDecline = "driver.cmd.trip_decline"
 	DriverCmdLocation    = "driver.cmd.location"
@@ -30,3 +30,4 @@ const (
 	// Payment commands (payment.cmd.*)
 	PaymentCmdCreateSession = "payment.cmd.create_session"
 )
+d
